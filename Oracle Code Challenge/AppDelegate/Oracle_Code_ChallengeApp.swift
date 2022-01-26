a76ee751f5f93ace257f316dbcd811075b542d27
Joel Andre Hernández Infante <joelandrehernandezinfante@MacBook-Pro-de-Joel.local>
2022-01-26 15:42:27 -0600

    initial repository configuration and general organization of the project

---
+++ Oracle Code Challenge/AppDelegate/Oracle_Code_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oracle_Code_ChallengeApp.swift
+//  Oracle Code Challenge
+//
+//  Created by Joel Andre Hernández Infante on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Oracle_Code_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
